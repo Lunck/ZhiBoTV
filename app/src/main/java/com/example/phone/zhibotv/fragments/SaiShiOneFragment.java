880c94e923b1e933412e675e1abdc963b6f33871
604902795 <604902795@qq.com>
2016-11-28 11:58:11 +0800

    ludi  saishi

--- app/src/main/java/com/example/phone/zhibotv/fragments/SaiShiOneFragment.java
+++ app/src/main/java/com/example/phone/zhibotv/fragments/SaiShiOneFragment.java
@@ -2,6 +2,8 @@ package com.example.phone.zhibotv.fragments;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,10 +16,30 @@ import com.example.phone.zhibotv.R;
  */
 public class SaiShiOneFragment extends BaseFragment {
     public static final String TAG=SaiShiOneFragment.class.getSimpleName();
+    private TabLayout mTablayout;
+    private ViewPager mViewPager;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
          inflate=inflater.inflate(R.layout.saishi_one_layout,container,false);
         return inflate;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        initView();
+        setupView();
+    }
+
+    private void setupView() {
+
+    }
+
+    private void initView() {
+        mTablayout = ((TabLayout) inflate.findViewById(R.id.saishi_tablayout));
+        mViewPager = ((ViewPager) inflate.findViewById(R.id.saizhi_viewpager));
+
     }
 }
